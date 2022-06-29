95f28f4f0de75f2bf2bd065967107eb35ada908d
Luciano Victor <lucianocarvallho7@gmail.com>
2022-06-29 10:16:28 -0300

    style: add svg

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import Square from './Icons/square.svg'
+import Circleball from './Icons/circleball.svg'
+import Circle from './Icons/circle.svg'
+import Pokeball from './Icons/pokeball.svg'
+
+export { Square, Circleball, Circle, Pokeball }
